bf05a37fa27b89060706dbdaba0ba4f507137ba5
William Logie <will.logie@anu.edu.au>
2021-07-10 00:35:51 +0200

    Plane strain and out_of_plane (spring) pressure validation done

--- test/solvers/tube/run_structural_tube_validation.py
+++ test/solvers/tube/run_structural_tube_validation.py
@@ -11,14 +11,17 @@ from neml import models, elasticity, parse
 
 import sys
 sys.path.append('../../..')
-from srlife import receiver, structural
+from srlife import receiver, structural, spring
 
 class TestCase:
-  def __init__(self, name, T, analytic_r, analytic_z, ri = 0.9, ro = 1.0,
-               h = 10.0, alpha = 1.0e-5, E = 100000.0, nu = 0.3, p = 1.0):
+  def __init__(self, name, T, analytic, z_force, z_strain = 0,
+               ri = 0.9, ro = 1.0, h = 10.0, alpha = 1.0e-5,
+               E = 100000.0, nu = 0.3, p = 1.0, dT = 0.0,
+               spring = False):
     self.name = name
     self.Tfn = T
     self.afn = analytic
+    self.fzfn = z_force
 
     self.ri = ri
     self.ro = ro
@@ -29,12 +32,22 @@ class TestCase:
     self.nu = nu
 
     self.p = p
+    self.dT = dT
+
+    self.sprung = spring
+    self.dz = z_strain * h
 
   def T(self, r):
-    return self.Tfn(r, self.ri, self.ro)
+    return self.Tfn(r, self.ri, self.ro, self.dT)
 
   def exact(self, r):
-    return self.afn(r, self.p, self.ri, self.ro, self.E, self.nu, self.alpha)
+    return self.afn(r, self.p, self.ri, self.ro, self.E, self.nu, self.dT, self.alpha)
+
+  def axial_force(self):
+    return self.fzfn(self.p, self.ri, self.ro)
+
+  def axial_disp(self):
+    return self.dzfn(self.h)
 
   def make_mat(self):
     emodel = elasticity.IsotropicLinearElasticModel(self.E, "youngs",
@@ -54,7 +67,7 @@ class TestCase:
 
     R, _, _ = tube.mesh
     Ts = np.zeros((2,) + R.shape[:dim])
-    Ts[1] = self.T(R)
+    Ts[1] = self.T(R[:,0,0])
 
     tube.add_results("temperature", Ts)
 
@@ -63,20 +76,46 @@ class TestCase:
 
     return tube
 
-  def run_comparison(self, dim, solver, axial_strain = 0, nr = 10,
-      nt = 20, nz = 10):
-    """
-    The axial_strain variable only works for 1D/2D, otherwise it's displacement.
-    """
+  def make_tube_spring(self, tube, smat, ssolver, axial_force):
+    network = spring.SpringNetwork()
+    network.add_node(0)
+    network.add_node(1)
+    network.add_edge(
+      0,1,
+      object = spring.TubeSpring(tube, ssolver, smat)
+    )
+
+    network.set_times([0,1])
+
+    network.validate_setup()
+
+    network.displacement_bc(0, lambda t: 0.0)
+    network.force_bc(1, lambda t: axial_force)
+
+    return network
+
+  def run_comparison(self, dim, solver,
+                     nr = 10, nt = 20, nz = 10):
     mat = self.make_mat()
     tube = self.make_tube(dim, nr, nt, nz)
 
     solver.setup_tube(tube)
     state_n = solver.init_state(tube, mat)
 
-    state_np1 = solver.solve(tube, 1, state_n, axial_strain)
-
-    solver.dump_state(tube, 1, state_np1)
+    if self.sprung:
+      """
+      Use a single spring network with imposed axial (pressure) force
+      """
+      network = self.make_tube_spring(tube, mat, solver, self.axial_force())
+      network.solve(1)
+      #print('Tube end displacement: %e' % network.displacements[1])
+      edge = list(network.edges(data=True))
+      spring = edge[0][2]['object']
+      state_np1 = spring.state_np1
+      solver.dump_state(spring.tube, 1, state_np1)
+    else:
+      state_np1 = solver.solve(tube, 1, state_n, self.dz)
+      solver.dump_state(tube, 1, state_np1)
 
     return state_np1, tube
 
@@ -116,39 +155,74 @@ class TestCase:
 
     return np.max(err), np.max(rel)
 
-def pressure_plane_strain_R_displ(r, p_i, r_i, r_o, E, nu, alpha):
+def pressure_plane_strain_R_disp(r, p, ri, ro, E, nu, dT, alpha):
   """
-  Assumption of plane strain in polar coordinates
+  Assumption of plane strain (axial strain = 0)
   """
-  A = r_i**2 * r_o**2 * -p_i / (r_o**2 - r_i**2)
-  C = p_i * r_i**2 / (r_o**2 - r_i**2)
+  A = ri**2 * ro**2 * -p / (ro**2 - ri**2)
+  C = p * ri**2 / (ro**2 - ri**2)
   dr = (-A*nu - A + C*r**2*(-2*nu**2 - nu + 1))/(E*r)
   return dr
 
-def pressure_gen_plane_strain_R_displ(r, p_i, r_i, r_o, E, nu, alpha):
+def thermal_plane_strain_R_disp(r, p, ri, ro, E, nu, dT, alpha):
   """
-  Assumption of generalised plane strain in polar coordinates
+  Constants of integration (stress) assume a linear temperature gradient
   """
-  A = r_i**2 * r_o**2 * -p_i / (r_o**2 - r_i**2)
-  C = p_i * r_i**2 / (r_o**2 - r_i**2)
-  dr = (-A*nu - A + C*r**2*(1 - 2*nu))/(E*r)
+  C_1 = -alpha*dT*(nu + 1)*(2*nu - 1)*\
+    (-ri**3/6 + ri*ro**2/2 - ro**3/3)/((nu - 1)*(ri - ro)**2*(ri + ro))
+  C_2 = alpha*dT*ri**2*(nu + 1)*\
+    (-ri**3/6 + ri*ro**2/2 - ro**3/3)/((nu - 1)*(ri - ro)**2*(ri + ro))
+  C_3 = 0
+  dr = C_1*r + C_2/r + C_3*nu*r/E + alpha*(nu + 1)*\
+    (-dT*r**3/(3*ri - 3*ro) + dT*r**2*ri/(2*ri - 2*ro) + \
+     dT*ri**3/(3*ri - 3*ro) - dT*ri**3/(2*ri - 2*ro))/(r*(1 - nu))
   return dr
 
-def pressure_gen_plane_strain_Z_strain(r, p_i, r_i, r_o, E, nu, alpha):
+def no_out_of_plane_Z_force(p, ri, ro):
   """
-  Assumption of gen. plane strain and cylinder closed at both ends
+  e.g. for use in generalised plane strain
   """
-  C = p_i * r_i**2 / (r_o**2 - r_i**2)
-  eps_z = (-2*C*nu + C)/E
-  return eps_z
+  return 0.0
+
+def pressure_out_of_plane_Z_force(p, ri, ro):
+  """
+  Assumption of tube closed at both ends (C is stress)
+  """
+  C = p * ri**2 / (ro**2 - ri**2)
+  return -C * np.pi * (ro**2 - ri**2)
+
+def pressure_out_of_plane_R_disp(r, p, ri, ro, E, nu, dT, alpha):
+  """
+  Assumption of tube closed at both ends
+  """
+  A = ri**2 * ro**2 * -p / (ro**2 - ri**2)
+  C = p * ri**2 / (ro**2 - ri**2)
+  dr = (-A*nu - A + C*r**2*(1 - 2*nu))/(E*r)
+  return dr
 
 if __name__ == "__main__":
 
   solver = structural.PythonTubeSolver(verbose = False)
 
   cases = [
-    TestCase("Pressure, plane strain", lambda T, ri, ro: 0.0, pressure_plane_strain_R_displ,
-             p = 100, ri = 8, ro = 10.0)
+    TestCase("Pressure, plane strain",
+             lambda r, ri, ro, dT: 0.0,
+             pressure_plane_strain_R_disp,
+             no_out_of_plane_Z_force,
+             p = 100, ri = 8, ro = 10.0,
+             spring = False),
+    TestCase("Pressure, incl. out of plane",
+             lambda r, ri, ro, dT: 0.0,
+             pressure_out_of_plane_R_disp,
+             pressure_out_of_plane_Z_force,
+             p = 100, ri = 8, ro = 10.0,
+             spring = True)# ,
+    # TestCase("Thermal, generalised plane strain",
+    #          lambda r, ri, ro, dT: 100 * (r - ri) / (ro - ri),
+    #          thermal_plane_strain_R_disp,
+    #          no_out_of_plane_Z_force,
+    #          p = 0, ri = 8, ro = 10.0,
+    #          spring = True)
   ]
 
   print("Analytical comparison")
@@ -158,7 +232,6 @@ if __name__ == "__main__":
   for d in range(1,2):
     for case in cases:
       state, tube = case.run_comparison(d, solver)
-
       a, r = case.evaluate_comparison(tube)
       print(case.name + ": " "%iD" % d)
       print("Axial force: %e" % state.force)
